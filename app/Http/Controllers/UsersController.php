2a27891c8b15e28272508b639223435e538ca33b
Yongcheng <yongcheng.chen@hotmail.com>
2019-09-10 10:22:18 +0000

    TICKET 列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,7 +28,11 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $tickets = $user->tickets()
+        			->orderBy('created_at', 'desc')
+        			->paginate(10);
+
+        return view('users.show', compact('user', 'tickets'));
     }
 
     public function store(Request $request)
